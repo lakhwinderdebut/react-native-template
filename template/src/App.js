c0d8967585bfc5f0e5359e96d074eca896ea249e
Lakhwinder Singh <lakhwindersingh@Lakhwinders-MacBook-Air.local>
2022-07-17 14:51:30 +0530

    APP.js moved to src folder

--- template/src/App.js
+++ template/src/App.js
@@ -1,11 +1,11 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import Store from './src/store/Store';
+import Store from './store/Store';
 import {Provider} from 'react-redux';
-import RootNavigator from './src/navigation';
+import RootNavigator from './navigation';
 import {I18nextProvider} from 'react-i18next';
 import i18next from 'i18next';
-import resources from './src/i18n/index';
+import resources from './i18n/index';
 
 const App = () => {
   // Multilingual support
